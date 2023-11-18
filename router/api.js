f3bdaa7b61bc62eb76b3e30435e5ba7d238ad8c2
NdaaTzyyy <96452047+NdaaTzyyy@users.noreply.github.com>
2023-11-18 12:26:53 +0000

    sanes

--- router/api.js
+++ router/api.js
@@ -2,11 +2,77 @@ var express = require("express")
 var scr = require("@xct007/frieren-scraper")
 var router = express()
 //made by NDaaaaaaa
-
-router.get("/instagram", async (req, res, next) => {
+router.get("/pinterest", async (req, res, next) => {
     let url = req.query.url
     if (!url) return res.json({"message":"url invalid!"})
-    const ress = await scr.instagram.v1(url)
+    const ress = await scr.pinterest.v1(url)
+    var result = ress
+    res.json({
+        result
+    })
+})
+router.get("/storywa-search", async (req, res, next) => {
+    let query = req.query.query
+    if (!query) return res.json({"message":"query invalid!"})
+    const ress = await scr.storyWa.search(query)
+    var result = ress
+    res.json({
+        result
+    })
+})
+router.get("/otakudesu-detail", async (req, res, next) => {
+    let url = req.query.url
+    if (!url) return res.json({"message":"url invalid!"})
+    const ress = await scr.otakudesu.detail(url)
+    var result = ress
+    res.json({
+        result
+    })
+})
+router.get("/otakudesu-search", async (req, res, next) => {
+    let query = req.query.query
+    if (!query) return res.json({"message":"query invalid!"})
+    const ress = await scr.otakudesu.search(query)
+    var result = ress
+    res.json({
+        result
+    })
+})
+router.get("/otakudesu-latest", async (req, res, next) => {
+    const ress = await scr.otakudesu.latest()
+    var result = ress
+    res.json({
+        result
+    })
+})
+//
+router.get("/komiku-detail", async (req, res, next) => {
+    let url = req.query.url
+    if (!url) return res.json({"message":"url invalid!"})
+    const ress = await scr.komikuId.detail(url)
+    var result = ress
+    res.json({
+        result
+    })
+})
+router.get("/komiku-search", async (req, res, next) => {
+    let query = req.query.query
+    if (!query) return res.json({"message":"query invalid!"})
+    const ress = await scr.komikuId.search(query)
+    var result = ress
+    res.json({
+        result
+    })
+})
+router.get("/komiku-latest", async (req, res, next) => {
+    const ress = await scr.komikuId.latest()
+    var result = ress
+    res.json({
+        result
+    })
+})
+router.get("/storywa-popular", async (req, res, next) => {
+    const ress = await scr.storyWa.popular()
     var result = ress
     res.json({
         result
