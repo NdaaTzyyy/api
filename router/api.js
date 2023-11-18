f02056621d3c0709ebf142cb7e94842d55a7f73f
NdaaTzyyy <96452047+NdaaTzyyy@users.noreply.github.com>
2023-11-18 12:03:41 +0000

    nw

--- router/api.js
+++ router/api.js
@@ -3,6 +3,15 @@ var scr = require("@xct007/frieren-scraper")
 var router = express()
 //made by NDaaaaaaa
 
+router.get("/instagram", async (req, res, next) => {
+    let url = req.query.url
+    if (!url) return res.json({"message":"url invalid!"})
+    const ress = await scr.instagram.v1(url)
+    var result = ress
+    res.json({
+        result
+    })
+})
 router.get("/tiktok", async (req, res, next) => {
     let url = req.query.url
     if (!url) return res.json({"message":"url invalid!"})
