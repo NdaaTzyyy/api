f99863976ab4459a1db19b67449b969bd8910cf0
NdaaTzyyy <96452047+NdaaTzyyy@users.noreply.github.com>
2023-11-18 10:41:20 +0000

    new

--- router/api.js
+++ router/api.js
@@ -3,3 +3,12 @@ var scr = require('@xct007/frieren-scraper')
 var fetch = require('node-fetch')
 var router = express;
 
+router.get("/tiktok", async (req, res, next) => {
+    let url = req.query.url
+    if (!url) return res.json({"message":"url invalid"})
+    const ress = await scr.tiktok(url)
+    var result = ress 
+    res.json({
+        result
+    })
+})
