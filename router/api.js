0a0d1b1dd21d4893068c4f10591cb0f0de690536
NdaaTzyyy <96452047+NdaaTzyyy@users.noreply.github.com>
2023-11-18 12:01:11 +0000

    nw

--- router/api.js
+++ router/api.js
@@ -6,8 +6,18 @@ var router = express()
 router.get("/tiktok", async (req, res, next) => {
     let url = req.query.url
     if (!url) return res.json({"message":"url invalid!"})
-    const ress = await scr.tiktok(url)
+    const ress = await scr.tiktok.v1(url)
     var result = ress
+    res.json({
+        result
+    })
+})
+router.get("/ip", async (req, res, next) => {
+    let query = req.query.query
+    if (!query) return res.json({"message":"query invalid!"})
+    const ress = await fetch(`https://ipapi.co/${query}/json`);
+    const rez = await ress.json()
+    var result = rez
     res.json({
         result
     })
